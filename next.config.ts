be096f599f44122877d686857216df7f6515589a
moonsi <anatjdls0716@gmail.com>
2025-07-09 11:07:00 +0900

    🐳 chore: next.js 환경설정 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,             // strict mode 활성화
-  swcMinify: true,                   // 빌드 속도 향상
   images: {
     domains: ['localhost'],          // 외부 이미지 사용
   },
