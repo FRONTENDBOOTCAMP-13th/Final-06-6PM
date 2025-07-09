b1e7f8ebfc525bd6ca45d664bc3ceccc6519a020
moonsi <anatjdls0716@gmail.com>
2025-07-09 10:02:59 +0900

    🐳 chore : 초기 개발환경 설정 (Next.js, ESLint, Prettier)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,             // strict mode 활성화
+  swcMinify: true,                   // 빌드 속도 향상
+  images: {
+    domains: ['localhost'],          // 외부 이미지 사용
+  },
 };
 
 export default nextConfig;
