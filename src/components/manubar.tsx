b6a9abd99ea8f1ba9c8eb3efd0b983e92dda3d00
namju1 <namju158@gmail.com>
2025-07-10 14:07:20 +0900

    💄 style: 메뉴바 색상 폰트 변수 수정

--- src/components/manubar.tsx
+++ src/components/manubar.tsx
@@ -4,37 +4,46 @@ import {
   SquarePen,
   Newspaper,
   UserRound,
+  LucideIcon,
 } from "lucide-react";
+
+// NavItem에 타입 명시!
+interface NavItemProps {
+  icon: LucideIcon; // 아이콘 컴포넌트
+  label: string;
+}
 
 export default function Menubar() {
   return (
-    <nav className="w-full max-w-[428px] h-[95px] bg-[#5FA185] flex justify-between items-center px-6 rounded-none mx-auto mt-10 shadow">
-      <NavItem icon={<Home size={36} color="#FFFFFF" />} label="홈" />
-      <NavItem
-        icon={<LayoutList size={36} color="#FFFFFF" />}
-        label="살펴보기"
-      />
-      <NavItem
-        icon={<SquarePen size={36} color="#FFFFFF" />}
-        label="기록하기"
-      />
-      <NavItem
-        icon={<Newspaper size={36} color="#FFFFFF" />}
-        label="커뮤니티"
-      />
-      <NavItem
-        icon={<UserRound size={36} color="#FFFFFF" />}
-        label="마이페이지"
-      />
+    <nav
+      className="w-full max-w-[428px] h-[95px] flex justify-between items-center px-6 rounded-none mx-auto mt-10 shadow"
+      style={{
+        backgroundColor: "var(--color-secondary)",
+      }}
+    >
+      <NavItem icon={Home} label="홈" />
+      <NavItem icon={LayoutList} label="살펴보기" />
+      <NavItem icon={SquarePen} label="기록하기" />
+      <NavItem icon={Newspaper} label="커뮤니티" />
+      <NavItem icon={UserRound} label="마이페이지" />
     </nav>
   );
 }
 
-function NavItem({ icon, label }: { icon: React.ReactNode; label: string }) {
+// NavItem 컴포넌트
+function NavItem({ icon: Icon, label }: NavItemProps) {
   return (
     <div className="flex flex-col items-center gap-1">
-      {icon}
-      <span className="text-white text-sm">{label}</span>
+      <Icon size={36} style={{ color: "var(--color-white)" }} />
+      <span
+        style={{
+          color: "var(--color-white)",
+          fontSize: "var(--text-14)",
+          fontFamily: "var(--font-sans)",
+        }}
+      >
+        {label}
+      </span>
     </div>
   );
 }
