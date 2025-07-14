45e3ea05494750f7d5be1c39d03474a824057a91
seonyoungg <tjsduddlfma1@naver.com>
2025-07-15 07:58:00 +0900

    💄style: 일정등록 컴포넌트 스타일 조정
    - 사용하지 않는 items-center 삭제
    - 배경색 조정
    - 글씨크기 조정
    - 버튼 크기 조정

--- src/components/ui/scheduleRegister.tsx
+++ src/components/ui/scheduleRegister.tsx
@@ -3,18 +3,15 @@ import ButtonRounded from "@/components/ui/btnRound";
 
 export default function ScheduleRegister() {
   return (
-    <div className="w-full bg-travel-bg100 rounded-2xl shadow-lg py-4 items-center">
-      {/* 일정을 등록해주세용 */}
-      <div className=" text-center">
-        <h2 className="text-20 mb-4 text-travel-text200">
+    <div className="w-full rounded-2xl border border-travel-gray200 bg-white py-8">
+      <div className="flex flex-col items-center">
+        {/* 일정을 등록해주세용 */}
+        <h2 className="text-16 mb-4 text-travel-text200">
           일정을 등록해주세요.
         </h2>
-      </div>
-
-      {/* 등록 버튼 */}
-      <div className=" flex justify-center">
+        {/* 등록 버튼 */}
         <ButtonRounded
-          size="lg"
+          size="md"
           variant="fill"
           className="flex items-center gap-1"
         >
