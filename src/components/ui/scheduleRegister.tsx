5aed4c2e5ddad842d56b0b258015602d3310b805
HyungJuCha <qtbreadlui@gmail.com>
2025-07-14 18:00:09 +0900

    💄style: 일정 등록 하기 컴포넌트 추가

--- src/components/ui/scheduleRegister.tsx
+++ src/components/ui/scheduleRegister.tsx
@@ -3,20 +3,22 @@ import ButtonRounded from "@/components/ui/btnRound";
 
 export default function ScheduleRegister() {
   return (
-    <div className="w-full max-w-md mx-auto bg-travel-bg100 rounded-2xl shadow-lg">
+    <div className="w-full bg-travel-bg100 rounded-2xl shadow-lg py-4">
       {/* 일정을 등록해주세용 */}
-      <div className="px-6 py-4 text-center">
-        <h2 className="text-20 text-travel-text200">일정을 등록해주세요.</h2>
+      <div className=" text-center">
+        <h2 className="text-20 mb-4 text-travel-text200">
+          일정을 등록해주세요.
+        </h2>
       </div>
 
       {/* 등록 버튼 */}
-      <div className="px-6 pb-6 flex justify-center">
+      <div className=" flex justify-center">
         <ButtonRounded
           size="lg"
           variant="fill"
           className="flex items-center gap-1"
         >
-          <Plus className="w-4 h-4" />
+          <Plus className="text-white w-4 h-4" />
           일정 등록하기
         </ButtonRounded>
       </div>
