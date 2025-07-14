727b02dca96b609be225b414edf228ac46e0b76e
seonyoungg <tjsduddlfma1@naver.com>
2025-07-15 08:16:02 +0900

    💄style: 색상 수정 및 id값 props제거

--- src/components/ui/planListItem.tsx
+++ src/components/ui/planListItem.tsx
@@ -5,21 +5,19 @@ export type PlanListItemProps = {
   tag: string;
 };
 
-const PlanListItem = ({ id, number, title, tag }: PlanListItemProps) => {
+const PlanListItem = ({ number, title, tag }: PlanListItemProps) => {
   return (
-    <div className="flex items-center gap-2 py-2">
+    <div className="flex items-center gap-2 text-travel-text100">
       {/* 숫자 */}
-      <div className="w-6 h-6 bg-travel-gray300 text-black rounded-full flex items-center justify-center text-14 font-bold">
+      <div className="w-6 h-6 bg-travel-gray200  rounded-full flex items-center justify-center text-14 font-bold">
         {number}
       </div>
 
       {/* 관광지명과 태그 */}
-      <div className="flex-1">
-        <span className="text-16 font-medium text-travel-text200">{title}</span>
-        <span className="text-10 ml-1 text-white bg-travel-primary200 px-1 py-1 rounded-4xl">
-          {tag}
-        </span>
-      </div>
+      <span className="text-16 font-medium">{title}</span>
+      <span className="text-10 text-white bg-travel-secondary200 px-2 py-1 rounded-4xl">
+        {tag}
+      </span>
     </div>
   );
 };
