0f7714e8bc5e2eb85623d90c3fab4c03f9d88fc5
namju1 <namju158@gmail.com>
2025-07-15 15:47:01 +0900

    💄 style: 파일명 카멜케이스로 통일, 코드 수정

--- src/components/ui/drawerSelectImg.tsx
+++ src/components/ui/drawerSelectImg.tsx
@@ -3,22 +3,21 @@
 import Button from "@/components/ui/btn";
 import { ChevronDown } from "lucide-react";
 
-export default function BtnDrawerItem() {
+export default function DrawerSelectImg() {
   const closeDrawer = () => {
     console.log("창 닫힘");
   };
 
   return (
     <div className="flex flex-col items-center w-full max-w-[430px] rounded-t-2xl bg-white shadow p-6 gap-4">
-      <button onClick={() => closeDrawer()} className="cursor-pointer">
+      <button onClick={closeDrawer} className="cursor-pointer">
         <ChevronDown className="w-6 h-6 text-travel-text100 mb-2" />
       </button>
-
+      <div className="w-full text-base font-medium text-travel-text100 mb-2">
+        선택한 지역명
+      </div>
       <Button size="lg" variant="primary" className="w-full">
-        게시글 수정하기
-      </Button>
-      <Button size="lg" variant="outline" className="w-full">
-        게시글 삭제하기
+        사진 선택하기
       </Button>
     </div>
   );
