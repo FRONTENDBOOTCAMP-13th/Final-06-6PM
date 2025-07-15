1383b440aa68961a80afc4ca47493a0af3de1889
seonyoungg <tjsduddlfma1@naver.com>
2025-07-15 13:29:35 +0900

    💄 style: 컴포넌트 스타일 조정 및 테스트 함수 추가

--- src/components/ui/btndrawer-item.tsx
+++ src/components/ui/btndrawer-item.tsx
@@ -1,22 +1,25 @@
+"use client";
+
+import Button from "@/components/ui/btn";
 import { ChevronDown } from "lucide-react";
 
 export default function BtnDrawerItem() {
-  return (
-    <div className="flex flex-col items-center w-full max-w-[428px] rounded-xl bg-white shadow p-4 gap-4">
-      <ChevronDown className="w-6 h-6 text-travel-text100 mb-2" />
+  const closeDrawer = () => {
+    console.log("창 닫힘");
+  };
 
-      <button
-        className="w-full border border-travel-primary100 text-travel-primary100 rounded-md py-3 font-medium bg-white hover:bg-gray-50 transition"
-        type="button"
-      >
+  return (
+    <div className="flex flex-col items-center w-full max-w-[430px] rounded-t-2xl bg-white shadow p-6 gap-4">
+      <button onClick={() => closeDrawer()} className="cursor-pointer">
+        <ChevronDown className="w-6 h-6 text-travel-text100 mb-2" />
+      </button>
+
+      <Button size="lg" variant="primary" className="w-full">
         게시글 수정하기
-      </button>
-      <button
-        className="w-full rounded-md py-3 font-medium text-white bg-travel-primary100 hover:bg-travel-primary200 transition"
-        type="button"
-      >
+      </Button>
+      <Button size="lg" variant="outline" className="w-full">
         게시글 삭제하기
-      </button>
+      </Button>
     </div>
   );
 }
