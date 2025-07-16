fc341bcea18b16e31319d12363017a0eb49cd009
ineahe <ineahe@gmail.com>
2025-07-16 15:34:49 +0900

    💄 style: 프로필편집 컴포넌트 제작

---
+++ src/app/mypage/profiledit/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import DayItem from "@/components/ui/dayItem";
+import ProfileCard from "@/components/ui/profileItemedit";
+import SelectMenu2 from "@/components/ui/selectMenu2";
+import BookmarkItem from "@/components/ui/bookmarkItem";
+
+export default function MypagePage() {
+  return (
+    <>
+      <div className="flex flex-col gap-5 items-center">
+        <div className="w-full">
+          <ProfileCard userName="여행덕후"></ProfileCard>
+        </div>
+        <div className="w-full flex flex-col gap-5">
+          <BookmarkItem type="bookmark" count={2} />
+          <BookmarkItem type="review" count={4} />
+        </div>
+      </div>
+      <div className="bg-white shadow-xl rounded-2xl overflow-hidden mt-5">
+        <SelectMenu2 />
+        <div className="p-4 grid grid-cols-1 gap-2">
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+        </div>
+      </div>
+    </>
+  );
+}
