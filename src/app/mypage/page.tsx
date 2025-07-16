56027db2b00df9fc4c51b2444f34c25457457cbc
ineahe <ineahe@gmail.com>
2025-07-16 15:32:49 +0900

    💄 style: 마이페이지 레이아웃 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,23 +1,31 @@
-import DayItem from "@/components/ui/dayItem";
-import ProfileCard from "@/components/ui/profileItem";
+"use client";
+
+import EditableProfileCard from "@/components/ui/profileItem";
 import SelectMenu2 from "@/components/ui/selectMenu2";
+import BookmarkItem from "@/components/ui/bookmarkItem";
+import DayItem from "@/components/ui/dayItem";
 
 export default function MypagePage() {
   return (
-    <div className="flex flex-col gap-8 items-center">
-      <div className="w-full">
-        <ProfileCard userName="여행덕후"></ProfileCard>
+    <>
+      <div className="flex flex-col gap-5 items-center">
+        <div className="w-full">
+          <EditableProfileCard userName="여행덕후" />
+        </div>
+        <div className="w-full flex flex-col gap-5">
+          <BookmarkItem type="bookmark" count={2} />
+          <BookmarkItem type="review" count={4} />
+        </div>
       </div>
-      <div className="w-full">
+      <div className="bg-white shadow-xl rounded-2xl overflow-hidden mt-5">
         <SelectMenu2 />
-      </div>
-      <div className="w-full">
-        <div className="space-y-4 w-full">
+        <div className="p-4 grid grid-cols-1 gap-2">
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
           <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
           <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
           <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
         </div>
       </div>
-    </div>
+    </>
   );
 }
