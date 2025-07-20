b360ffb31d44966e3f9704ce859a5e88171656c6
seonyoungg <tjsduddlfma1@naver.com>
2025-07-21 02:43:52 +0900

    💄 style: 3뎁스에 따른 페이지 데이터 구현

--- src/components/form/reviewSelect.tsx
+++ src/components/form/reviewSelect.tsx
@@ -3,12 +3,12 @@
 import { CalendarDays, ChevronDown } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
-interface DayItem {
+export interface DayItem {
   days: string;
   place: string[] | string;
 }
 
-interface ReviewSelectProps {
+export interface ReviewSelectProps {
   list: DayItem[];
   selected: DayItem;
   onChange: (item: DayItem) => void;
@@ -38,12 +38,12 @@ export default function ReviewSelect({
         className="flex items-center justify-between px-4 py-2 bg-white border rounded-lg cursor-pointer text-travel-text100 border-travel-gray400"
         onClick={() => setOpen((prev) => !prev)}
       >
-        <div>
+        <div className="space-y-1">
           <p className="flex items-center gap-1 font-medium text-16">
             <CalendarDays />
             <span>{selected.days}</span>
           </p>
-          <p>
+          <p className="line-clamp-1">
             방문 장소:{" "}
             {Array.isArray(selected.place)
               ? selected.place.join(", ")
@@ -54,7 +54,7 @@ export default function ReviewSelect({
       </div>
 
       {open && (
-        <ul className="absolute top-[59px] left-0 w-full border rounded-lg bg-white shadow-xl z-10 max-h-60 overflow-auto">
+        <ul className="absolute top-[64px] left-0 w-full border rounded-lg bg-white shadow-xl z-10 max-h-64 overflow-auto">
           {list.map((item) => (
             <li
               key={
@@ -71,7 +71,7 @@ export default function ReviewSelect({
                 <CalendarDays />
                 <span>{item.days}</span>
               </p>
-              <p>
+              <p className="line-clamp-1">
                 방문 장소:{" "}
                 {Array.isArray(item.place) ? item.place.join(", ") : item.place}
               </p>
