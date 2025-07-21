25ed535c282229fd7501b8616025956b2515d0b3
seonyoungg <tjsduddlfma1@naver.com>
2025-07-21 23:18:53 +0900

    🐳 chore : 공공데이터 출력 연습

--- src/data/actions/travel.ts
+++ src/data/actions/travel.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import { fetchTravel } from "@/data/functions/fetchTravel";
-import { FetchAreaProps, FetchAreaTravelProps } from "@/types/travel";
+import {
+  FetchAreaProps,
+  FetchAreaTravelProps,
+  FetchKeywordProps,
+} from "@/types/travel";
 
 const API_KEY = process.env.NEXT_PUBLIC_TOUR_API_KEY!;
 
@@ -37,6 +41,39 @@ export async function fetchTravelList(
   };
 
   const res = await fetchTravel("/areaBasedList2", params);
-  // console.log(`Travel res`, res);
+  console.log(`Travel res`, res);
+  return res.response;
+}
+
+// 키워드 기반 데이터 가져오기
+export async function fetchKeywordData(
+  keyword: string
+): Promise<FetchKeywordProps> {
+  const params = {
+    serviceKey: API_KEY,
+    keyword,
+    MobileOS: "ETC",
+    MobileApp: "TravelDiary",
+    pageNo: "1",
+    numOfRows: "10",
+    _type: "json",
+  };
+
+  const res = await fetchTravel("/searchKeyword2", params);
+  return res.response;
+}
+
+// 콘텐츠 ID 기반 데이터 가져오기
+export async function fetchContentData(contentId: string) {
+  const params = {
+    serviceKey: API_KEY,
+    MobileOS: "ETC",
+    MobileApp: "TravelDiary",
+    contentId: String(contentId),
+    pageNo: "1",
+    numOfRows: "10",
+    _type: "json",
+  };
+  const res = await fetchTravel("/detailCommon2", params);
   return res.response;
 }
