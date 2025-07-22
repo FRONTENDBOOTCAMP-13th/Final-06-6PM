62b8790c273106952aaa0070e7bebc2a3af1599b
HyungJuCha <qtbreadlui@gmail.com>
2025-07-22 17:46:34 +0900

    🧪 test : 지도 초기 좌표값 및 클릭이벤트 추가

--- src/app/photomap/view/page.tsx
+++ src/app/photomap/view/page.tsx
@@ -1,15 +1,16 @@
 import { ZoomIn, ZoomOut } from "lucide-react";
+import KoreaMap from "./mapcom/koreaMap";
 
 // 지도생성 뷰
 export default function PhotoMapViewPage() {
   return (
     <>
-      <div className="w-full min-h-[600px] bg-amber-200 mt-4">
-        지도 들어올 공간
-      </div>
-      <div className="flex items-center justify-end w-full gap-2 py-2">
+      <div>
         <ZoomIn />
         <ZoomOut />
+        <div className="w-full bg-amber-200 mt-4">
+          <KoreaMap />
+        </div>
       </div>
     </>
   );
