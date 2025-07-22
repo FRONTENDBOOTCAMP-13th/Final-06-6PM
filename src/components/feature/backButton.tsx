1cbd8e5666268781d9f749d64046c5c48574ec63
SeOinm <anatjdls0716@gmail.com>
2025-07-22 23:33:42 +0900

    ♻️ refactor: 버튼 컴포넌트 분리

--- src/components/feature/backButton.tsx
+++ src/components/feature/backButton.tsx
@@ -5,17 +5,15 @@ import { useRouter } from "next/navigation";
 
 interface BackButtonProps {
   path?: string;
+  onPrev?: () => void;
 }
 
-export default function BackButton({ path }: BackButtonProps) {
+export default function BackButton({ path, onPrev }: BackButtonProps) {
   const router = useRouter();
 
   const goBackPage = () => {
-    if (path) {
-      router.push(path);
-    } else {
-      router.back();
-    }
+    if (onPrev) return onPrev();
+    if (path) return router.push(path);
   };
 
   return (
