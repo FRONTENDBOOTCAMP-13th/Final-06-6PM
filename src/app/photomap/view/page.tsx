72b40baa16da0c9bc8fee46975bac3365395c322
HyungJuCha <qtbreadlui@gmail.com>
2025-07-23 02:07:01 +0900

    🧪 test : 지도 세분화 코드 추가

--- src/app/photomap/view/page.tsx
+++ src/app/photomap/view/page.tsx
@@ -1,5 +1,6 @@
 import { ZoomIn, ZoomOut } from "lucide-react";
 import KoreaMap from "./mapcom/koreaMap";
+import TestMap from "@/app/photomap/view/mapcom/depth/paths";
 
 // 지도생성 뷰
 export default function PhotoMapViewPage() {
@@ -10,6 +11,9 @@ export default function PhotoMapViewPage() {
         <ZoomOut />
         <div className="w-full bg-amber-200 mt-4">
           <KoreaMap />
+        </div>
+        <div className="w-full bg-amber-200 mt-4">
+          <TestMap />
         </div>
       </div>
     </>
