4aa74c1b4cc799460072d3c031fa4a063040e7a4
seonyoungg <tjsduddlfma1@naver.com>
2025-07-22 17:25:28 +0900

    ✨ feat: 로그인 상태 관련 페이지 조건 처리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,9 +1,36 @@
+// app/mypage/page.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import useUserStore from "@/zustand/userStore";
 import ProfileItem from "@/components/ui/profileItem";
 import BookmarkItem from "@/components/ui/bookmarkItem";
 import Link from "next/link";
 import SelectMypage from "@/components/feature/selectMypage";
 
 export default function MypagePage() {
+  const { user } = useUserStore();
+  const router = useRouter();
+  const [userCheck, setUserCheck] = useState(true); // 로딩 상태
+
+  useEffect(() => {
+    // 로그인 상태 최종 확인
+    if (user === null) {
+      router.replace("/login");
+    } else {
+      setUserCheck(false); // 로그인되어 있으면 로딩 종료
+    }
+  }, [user, router]);
+
+  if (userCheck) {
+    return (
+      <div className="h-screen flex justify-center items-center w-full pb-40">
+        <div className="w-20 h-20 border-4 border-travel-primary200 border-t-transparent rounded-full animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="flex flex-col gap-5 items-center">
