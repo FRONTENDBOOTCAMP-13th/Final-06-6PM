4d88efe953da09cc074d2f8641aefd7ae0c2a48a
seonyoungg <tjsduddlfma1@naver.com>
2025-07-23 01:46:23 +0900

    ♻️ refactor: 마이페이지 컴포넌트 데이터 출력 관련 재조정

--- src/components/ui/profileItem.tsx
+++ src/components/ui/profileItem.tsx
@@ -1,31 +1,39 @@
 import DrawerMypage from "@/components/feature/drawerMypage";
+import { getUser } from "@/data/actions/user";
+import { User } from "@/types/user";
 import useUserStore from "@/zustand/userStore";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_SERVER;
 
 export interface ProfileItemProps {
-  imgUrl?: string;
-  userName: string;
-  desc?: string;
   postsCount?: number;
   likesCount?: number;
   totalLikes?: number;
 }
 
 export default function ProfileItem({
-  imgUrl = "/images/user2.png",
-  userName = "사용자",
-  desc = "사용자 소개글입니다. 사용자 소개글입니다. 사용자 소개글입니다. 사용자 소개글입니다.",
   postsCount = 0,
   likesCount = 0,
   totalLikes = 0,
 }: ProfileItemProps) {
-  const user = useUserStore((state) => state.user);
-  const imageUrl = user?.image?.startsWith("http")
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  const user = useUserStore((state) => state.userInfo);
+  const imgUrl = user?.image?.startsWith("http")
     ? user.image
     : `${API_URL}/${user?.image}`;
+
+  // 사용자 정보
+  useEffect(() => {
+    const userId = user!._id;
+    const userData = async () => {
+      const res = await getUser(userId);
+      setUserInfo(res.item);
+    };
+    userData();
+  }, []);
 
   return (
     <div className="relative flex flex-col items-center gap-4 px-5 py-8 font-sans text-center bg-white shadow rounded-xl">
@@ -33,12 +41,12 @@ export default function ProfileItem({
 
       {/* 프로필이미지 */}
       <div className="overflow-hidden rounded-full w-25 h-25 bg-travel-gray200 aspect-square">
-        {imageUrl && (
+        {imgUrl && (
           <Image
             width={100}
             height={100}
-            src={imageUrl || "/images/front-end.png"}
-            alt={userName}
+            src={imgUrl || "/images/user1.png"}
+            alt={user?.name || "사용자"}
             className="object-cover w-full h-full"
           />
         )}
@@ -48,7 +56,7 @@ export default function ProfileItem({
       <div className="space-y-1">
         <h2 className="font-semibold text-20">{user?.name}</h2>
         <p className="px-3 break-keep text-travel-gray700 line-clamp-3">
-          {desc}
+          {userInfo?.desc || "소개글이 없습니다."}
         </p>
       </div>
 
