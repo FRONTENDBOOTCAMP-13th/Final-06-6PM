bb0db6b188a6c3e74f6272fc68f8e118c55001ac
seonyoungg <tjsduddlfma1@naver.com>
2025-07-22 16:07:36 +0900

    ✨ feat: 로그인 상태 관련 네비바 컴포넌트에서 확인 이후 페이지 이동 처리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,36 +1,9 @@
-"use client";
 import ProfileItem from "@/components/ui/profileItem";
 import BookmarkItem from "@/components/ui/bookmarkItem";
 import Link from "next/link";
 import SelectMypage from "@/components/feature/selectMypage";
-import useUserStore from "@/zustand/userStore";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function MypagePage() {
-  const { user } = useUserStore(); //로그인 여부 확인
-  const router = useRouter();
-
-  const [checkUser, setCheckUser] = useState(true); // 로딩 중 상태
-
-  useEffect(() => {
-    // user === null 로그아웃 상태 => 로그인 페이지 이동
-    if (user === null) {
-      router.replace("/login");
-    } else {
-      // user !== null 로그인 상태
-      setCheckUser(false);
-    }
-  }, [user, router]);
-
-  if (checkUser) {
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <div className="w-10 h-10 border-4 border-travel-primary200 border-t-transparent rounded-full animate-spin" />
-      </div>
-    );
-  }
-
   return (
     <>
       <div className="flex flex-col gap-5 items-center">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
-import { Home, LayoutList, Map, SquarePen, UserRound } from "lucide-react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import useUserStore from "@/zustand/userStore";
 import Link from "next/link";
+import { UserRound, Home, LayoutList, SquarePen, Map } from "lucide-react";
 
 export default function Navbar() {
+  const { user } = useUserStore();
+  const router = useRouter();
+
+  const userStatusCheck = (e: React.MouseEvent) => {
+    if (!user) {
+      e.preventDefault();
+      router.push("/login");
+    }
+  };
+
   return (
     <nav className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] p-4 bg-travel-primary-light100 max-h-21 z-20">
       <div className="grid items-center grid-cols-5 gap-3 mx-auto w-fit">
@@ -21,6 +35,7 @@ export default function Navbar() {
         </Link>
         <Link
           href="/plan"
+          onClick={userStatusCheck}
           className="flex flex-col items-center gap-2 text-white text-14 max-[500px]:text-12"
         >
           <SquarePen />
@@ -28,6 +43,7 @@ export default function Navbar() {
         </Link>
         <Link
           href="/photomap"
+          onClick={userStatusCheck}
           className="flex flex-col items-center gap-2 text-white text-14 max-[500px]:text-12"
         >
           <Map />
@@ -35,6 +51,7 @@ export default function Navbar() {
         </Link>
         <Link
           href="/mypage"
+          onClick={userStatusCheck}
           className="flex flex-col items-center gap-2 text-white text-14 max-[500px]:text-12"
         >
           <UserRound />
