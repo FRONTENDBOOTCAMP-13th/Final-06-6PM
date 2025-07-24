7df629b0fa98a92eccd5cf6803a2fe885da8730b
seonyoungg <tjsduddlfma1@naver.com>
2025-07-24 17:00:52 +0900

    💄 style: 폼 내부 컴포넌트 스타일 재조정

--- src/components/form/reviewImg.tsx
+++ src/components/form/reviewImg.tsx
@@ -1,10 +1,10 @@
-import { ImagePlus, Plane } from "lucide-react";
+import { Camera, ImagePlus } from "lucide-react";
 
 export default function ReviewImg() {
   return (
-    <div className="space-y-2">
+    <div className="space-y-1.5">
       <div className="flex items-center gap-1.5">
-        <Plane />
+        <Camera className="size-5" />
         <h3 className="font-semibold text-18">사진첨부</h3>
       </div>
       <div className="p-4 bg-white border rounded-lg b text-travel-gray700 border-travel-gray400 w-fit">
