34740894fc70c61ea61f3379c52d4ec72b641ed6
ineahe <ineahe@gmail.com>
2025-07-28 14:20:30 +0900

    🐳 chore: Next.js 15 비동기 params 처리 방식 적용

--- src/app/feed/[id]/page.tsx
+++ src/app/feed/[id]/page.tsx
@@ -1,11 +1,12 @@
 import FeedDetailContent from "@/components/feature/feedDetailContent";
 
 interface FeedViewPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
-export default function FeedViewPage({ params }: FeedViewPageProps) {
-  return <FeedDetailContent reviewId={params.id} />;
+export default async function FeedViewPage({ params }: FeedViewPageProps) {
+  const { id } = await params;
+  return <FeedDetailContent reviewId={id} />;
 }
