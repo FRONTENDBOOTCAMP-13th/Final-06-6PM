0e88b5b90d11112dbf378b9d8d4e1a65370c2f65
ineahe <ineahe@gmail.com>
2025-07-30 09:34:45 +0900

    ♻️ refactor: ApiRes 타입 정의 api 파일로 분리

--- src/data/actions/reply.ts
+++ src/data/actions/reply.ts
@@ -1,41 +1,30 @@
 "use server";
-
 import { revalidatePath } from "next/cache";
 import { ReviewReply } from "@/types/review";
+import { ApiReplyRes, ApiReplyResPromise } from "@/types/api";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://fesp-api.koyeb.app/market";
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || "febc13-final06-emjf";
 
-interface ApiRes<T> {
-  ok: number;
-  message?: string;
-  item?: T;
-  errors?: any;
-}
-
-type ApiResPromise<T> = Promise<ApiRes<T>>;
-
 /**
  * 리뷰 댓글을 생성하는 함수
- * @param {ApiRes<ReviewReply> | null} state - 이전 상태(사용하지 않음)
+ * @param {ApiReplyRes<ReviewReply> | null} state - 이전 상태(사용하지 않음)
  * @param {FormData} formData - 댓글 정보를 담은 FormData 객체
- * @returns {Promise<ApiRes<ReviewReply>>} - 생성 결과 응답 객체
+ * @returns {Promise<ApiReplyRes<ReviewReply>>} - 생성 결과 응답 객체
  * @description
  * 댓글을 생성하고, 성공 시 해당 게시글의 댓글 목록을 갱신합니다.
  */
 export async function createReviewReply(
-  state: ApiRes<ReviewReply> | null,
+  state: ApiReplyRes<ReviewReply> | null,
   formData: FormData,
-): ApiResPromise<ReviewReply> {
+): ApiReplyResPromise<ReviewReply> {
   const body = {
     content: formData.get("content"),
   };
-
   const _id = formData.get("_id");
   const accessToken = formData.get("accessToken");
-
   let res: Response;
-  let data: ApiRes<ReviewReply>;
+  let data: ApiReplyRes<ReviewReply>;
 
   try {
     res = await fetch(`${API_URL}/posts/${_id}/replies`, {
@@ -47,7 +36,6 @@ export async function createReviewReply(
       },
       body: JSON.stringify(body),
     });
-
     data = await res.json();
   } catch (error) {
     console.error(error);
