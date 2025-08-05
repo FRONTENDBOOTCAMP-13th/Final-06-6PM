3989c90df5b00ce203d2f9fbe3f53280a81741df
seonyoungg <tjsduddlfma1@naver.com>
2025-08-05 11:11:16 +0900

    ✨ feat: 나의 리뷰 개수 조회

--- src/components/form/mypage/bookAndReview.tsx
+++ src/components/form/mypage/bookAndReview.tsx
@@ -5,12 +5,14 @@ import { useEffect, useState } from "react";
 import useUserStore from "@/zustand/userStore";
 import BookmarkItem from "@/components/mypage/bookmarkItem";
 import { getReviewAllList, getReviewDailyList, getReviewPlaceList } from "@/data/functions/review";
+import { getReviewAllUser, getReviewDailyUser, getReviewPlaceUser } from "@/lib/api/review";
 
 export default function BookAndReview() {
   const { isLoggedIn, token } = useUserStore();
 
   // 카운트 상태
   const [bookmarkCount, setBookmarkCount] = useState(0);
+  const [reviewCount, setReviewCount] = useState(0);
 
   // 카운트 조회 함수
   const fetchCounts = async () => {
@@ -40,19 +42,46 @@ export default function BookAndReview() {
     }
   };
 
-  useEffect(() => {
-    // 카운트 조회 함수
-    if (isLoggedIn && token) {
-      fetchCounts();
+  // 나의 리뷰 개수 조회
+  const ReviewCounts = async () => {
+    if (!token) return;
+
+    try {
+      const [reviewAllRes, reviewDailyRes, reviewPlaceRes] = await Promise.all([
+        getReviewAllUser(token),
+        getReviewDailyUser(token),
+        getReviewPlaceUser(token),
+      ]);
+      const reviewAllData = reviewAllRes?.ok === 1 ? reviewAllRes.item || [] : [];
+      const reviewDailyData = reviewDailyRes?.ok === 1 ? reviewDailyRes.item || [] : [];
+      const reviewPlaceData = reviewPlaceRes?.ok === 1 ? reviewPlaceRes.item || [] : [];
+
+      // 각 리뷰별 개수 조회
+      const reviewAllLength = reviewAllData.length;
+      const reviewDailyLength = reviewDailyData.length;
+      const reviewPlaceLength = reviewPlaceData.length;
+      const totalReviewCount = reviewAllLength + reviewDailyLength + reviewPlaceLength;
+
+      setReviewCount(totalReviewCount);
+    } catch (error) {
+      console.error("나의 리뷰 조회 에러", error);
     }
-  }, [isLoggedIn, token]);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchCounts();
+      ReviewCounts();
+    }
+  }, [token]);
+
   return (
     <div className="flex flex-col w-full gap-4">
       <Link href="/mypage/bookmark">
         <BookmarkItem type="bookmark" count={bookmarkCount} />
       </Link>
       <Link href="/mypage/review">
-        <BookmarkItem type="review" count={4} />
+        <BookmarkItem type="review" count={reviewCount} />
       </Link>
     </div>
   );
