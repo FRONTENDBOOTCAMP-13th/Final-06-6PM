fed325f97d74b6869868795d3cbda9e73fd47657
seonyoungg <tjsduddlfma1@naver.com>
2025-08-06 11:32:59 +0900

    ✨ feat: 공휴하기 버튼 추가

---
+++ src/components/feature/shareBtn.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Share2 } from "lucide-react";
+export default function ShareBtn() {
+  return (
+    <>
+      <button>
+        <Share2 className="cursor-pointer size-5 leading-none text-travel-gray400" />
+      </button>
+    </>
+  );
+}
